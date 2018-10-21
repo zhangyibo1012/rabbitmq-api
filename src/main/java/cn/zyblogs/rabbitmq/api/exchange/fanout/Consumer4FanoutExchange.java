b33b403b6ea96d464e3e723735d45818a902cc24
zhangyibo1012 <35024391+zhangyibo1012@users.noreply.github.com>
2018-10-21 11:20:35 +0800

    fanout topic

---
+++ src/main/java/cn/zyblogs/rabbitmq/api/exchange/fanout/Consumer4FanoutExchange.java
@@ -0,0 +1,59 @@
+package cn.zyblogs.rabbitmq.api.exchange.fanout;
+
+import com.rabbitmq.client.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Title: Consumer4FanoutExchange.java
+ * @Package cn.zyblogs.rabbitmq.api.exchange.fanout
+ * @Description: TODO
+ * @Author ZhangYB
+ * @Version V1.0
+ */
+@Slf4j
+public class Consumer4FanoutExchange {
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory connectionFactory = new ConnectionFactory() ;
+
+        connectionFactory.setHost("192.168.32.143");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+
+        // 是否支持自动重连
+        connectionFactory.setAutomaticRecoveryEnabled(true);
+        connectionFactory.setNetworkRecoveryInterval(3000);
+        Connection connection = connectionFactory.newConnection();
+        log.info("==========Consumer连接成功==========");
+        Channel channel = connection.createChannel();
+
+        // 声明
+        String exchangeName = "test_fanout_exchange";
+        String exchangeType = "fanout";
+        String queueName = "test_fanout_queue";
+        // 不设置路由键
+        String routingKey = "";
+
+        // 声明了一个交换机
+        channel.exchangeDeclare(exchangeName, exchangeType, true, false, false, null);
+
+        // 声明了一个队列
+        channel.queueDeclare(queueName, false, false, false, null);
+
+        // 建立一个绑定关系
+        channel.queueBind(queueName, exchangeName, routingKey);
+
+        // 设置channel  true 是否自动接收   new DefaultConsumer 消费者
+        channel.basicConsume(queueName, true, "myConsumerTag" , new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.err.println("消费端:" + new String(body));
+                long deliveryTag = envelope.getDeliveryTag();
+                channel.basicAck(deliveryTag, false);
+            }
+        });
+    }
+}
---
+++ src/main/java/cn/zyblogs/rabbitmq/api/exchange/fanout/Producer4FanoutExchange.java
@@ -0,0 +1,54 @@
+package cn.zyblogs.rabbitmq.api.exchange.fanout;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.IntStream;
+
+/**
+ * @Title: Producer4FanoutExchange.java
+ * @Package cn.zyblogs.rabbitmq.api.exchange.fanout
+ * @Description: TODO  不处理路由键 只需要简单的将队列绑定到交换机上
+ *                     发送到交换机的消息都会被转发到与该交换机绑定的所有队列上
+ *                     Fanout交换机转发消息最快 不会走路由
+ *
+ * @Author ZhangYB
+ * @Version V1.0
+ */
+@Slf4j
+public class Producer4FanoutExchange {
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        // 创建一个ConnectionFactory进行配置
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.32.143");
+        connectionFactory.setPort(5672);
+        // 设置虚拟主机
+        connectionFactory.setVirtualHost("/");
+
+        // 通过连接工厂获得连接
+        final Connection connection = connectionFactory.newConnection();
+        log.info("==========Consumer连接成功==========");
+        // 通过connection创建一个Channel
+        final Channel channel = connection.createChannel();
+
+        // 声明
+        String exchangeName = "test_fanout_exchange";
+
+        // 发送10次数据
+        IntStream.rangeClosed(1,10 ).forEach((c) -> {
+            String msg = "Hello World RabbitMQ 4  Direct Exchange Message  ... ";
+            try {
+                channel.basicPublish(exchangeName, "zz" , null , msg.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        channel.close();
+        connection.close();
+    }
+}
